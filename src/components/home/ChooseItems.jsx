7efbfc74f062ec358e909510c63973c140d03ab9
Kendrick Halder <108589219+halderkendrick@users.noreply.github.com>
2024-06-05 15:04:23 +0600

    Update ChooseItems.jsx

--- src/components/home/ChooseItems.jsx
+++ src/components/home/ChooseItems.jsx
@@ -1,6 +1,6 @@
 const ChooseItems = ({ children }) => {
   return (
-    <div className="py-11 pl-7 rounded-xl flex gap-9 items-start transition-all hover:shadow-lg">
+    <div className="py-11 pl-7 rounded-xl flex gap-9 items-start transition-all hover:shadow-lg cursor-pointer">
       <span className="relative mt-2 w-6 h-6 rounded-full bg-brand after:absolute after:top-0 after:left-1/2 after:w-6 after:h-6 after:rounded-full after:bg-[#CBC8F0]"></span>
       <p className="text-lg font-normal text-secondary w-[350px]">{children}</p>
     </div>
